03578a96f66ead52c256ea9ce4d17122917856cb
KoenieFrankenstein <koen.frankena@student.hu.nl>
2020-01-06 12:15:32 +0100

    reserveren naar database

--- routes/api.js
+++ routes/api.js
@@ -2,4 +2,39 @@ const router = require('express').Router();
 var NoSQL = require('nosql');
 var db = NoSQL.load('./local.db.nosql');
 
+router.get('/api/hello', (req, res) => {
+    const obj = {
+        foo: 'bar'
+    };
+
+    db.insert({
+        name: 'New user',
+        email: 'new@example.com',
+        password: 'foobar'
+    });
+
+    res.json(obj);
+    
+});
+
+router.post('/reserveren', (req, res) => {
+
+    var vandaag = req.body.vandaag;
+    var kapper = req.body.kappers;
+    var tijd = req.body.selectorTime
+    var naam = req.body.naampie;
+    var email = req.body.email;
+
+    db.insert({
+        datum: vandaag,
+        kapper: kapper,
+        tijd: tijd,
+        naam: naam,
+        email: email,
+    }); 
+
+    res.redirect("index.html");
+
+})
+
 module.exports = router;
